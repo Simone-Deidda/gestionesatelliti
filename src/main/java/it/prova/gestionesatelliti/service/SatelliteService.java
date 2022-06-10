df1eb9638f48fca140c590224146cf26eb48f16e
Simone-Deidda <deidda.simone94@gmail.com>
2022-06-10 15:56:39 +0200

    Aggiunte le classi Repository e Service di Satellite

---
+++ src/main/java/it/prova/gestionesatelliti/service/SatelliteService.java
@@ -0,0 +1,19 @@
+package it.prova.gestionesatelliti.service;
+
+import java.util.List;
+
+import it.prova.gestionesatelliti.model.Satellite;
+
+public interface SatelliteService {
+	public List<Satellite> listAllElements();
+
+	public Satellite caricaSingoloElemento(Long id);
+
+	public void aggiorna(Satellite satelliteInstance);
+
+	public void inserisciNuovo(Satellite satelliteInstance);
+
+	public List<Satellite> findByExample(Satellite example);
+
+	public void rimuovi(Long idImpiegato);
+}
---
+++ src/main/java/it/prova/gestionesatelliti/service/SatelliteServiceImpl.java
@@ -0,0 +1,81 @@
+package it.prova.gestionesatelliti.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.Predicate;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import it.prova.gestionesatelliti.model.Satellite;
+import it.prova.gestionesatelliti.repository.SatelliteRepository;
+
+@Service
+public class SatelliteServiceImpl implements SatelliteService {
+	@Autowired
+	private SatelliteRepository satelliteRepository;
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Satellite> listAllElements() {
+		return (List<Satellite>) satelliteRepository.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Satellite caricaSingoloElemento(Long id) {
+		return satelliteRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public void aggiorna(Satellite satelliteInstance) {
+		satelliteRepository.save(satelliteInstance);
+	}
+
+	@Override
+	@Transactional
+	public void inserisciNuovo(Satellite satelliteInstance) {
+		satelliteRepository.save(satelliteInstance);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Satellite> findByExample(Satellite example) {
+		Specification<Satellite> specificationCriteria = (root, query, cb) -> {
+
+			List<Predicate> predicates = new ArrayList<Predicate>();
+
+			if (StringUtils.isNotEmpty(example.getDenominazione()))
+				predicates.add(cb.like(cb.upper(root.get("denominazione")),
+						"%" + example.getDenominazione().toUpperCase() + "%"));
+
+			if (StringUtils.isNotEmpty(example.getCodice()))
+				predicates.add(cb.like(cb.upper(root.get("codice")), "%" + example.getCodice().toUpperCase() + "%"));
+
+			if (example.getStato() != null)
+				predicates.add(cb.equal(root.get("stato"), example.getStato()));
+
+			if (example.getDataLancio() != null)
+				predicates.add(cb.greaterThanOrEqualTo(root.get("dataDiLancio"), example.getDataLancio()));
+
+			if (example.getDataRientro() != null)
+				predicates.add(cb.greaterThanOrEqualTo(root.get("dataDiRientro"), example.getDataRientro()));
+
+			return cb.and(predicates.toArray(new Predicate[predicates.size()]));
+		};
+
+		return satelliteRepository.findAll(specificationCriteria);
+	}
+
+	@Override
+	@Transactional
+	public void rimuovi(Long idImpiegato) {
+		satelliteRepository.deleteById(idImpiegato);
+	}
+
+}
