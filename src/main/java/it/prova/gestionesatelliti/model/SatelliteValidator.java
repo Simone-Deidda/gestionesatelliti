065a009f45ed6cd18ac7a17f0d2071b3ab845d8c
Simone-Deidda <deidda.simone94@gmail.com>
2022-06-10 19:55:57 +0200

    Implementata InsertSatellite con custom validation

---
+++ src/main/java/it/prova/gestionesatelliti/model/SatelliteValidator.java
@@ -0,0 +1,38 @@
+package it.prova.gestionesatelliti.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class SatelliteValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Satellite.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Satellite satellite = (Satellite) target;
+
+		if (satellite.getDataLancio() != null && satellite.getDataRientro() != null
+				&& satellite.getDataLancio().after(satellite.getDataRientro())) {
+			errors.rejectValue("dataLancio", "dataLancio.after.dataRientro");
+		}
+
+		if (satellite.getDataLancio() == null && satellite.getDataRientro() != null) {
+			errors.rejectValue("dataRientro", "dataRientro.exists.without.dataLancio");
+		}
+
+		if ((satellite.getStato() == StatoSatellite.IN_MOVIMENTO || satellite.getStato() == StatoSatellite.FISSO)
+				&& satellite.getDataRientro() != null) {
+			errors.rejectValue("stato", "dataRientro.exists.with.Stato.not.DISATTIVATO");
+		}
+
+		if (satellite.getStato() == StatoSatellite.DISATTIVATO && satellite.getDataRientro() == null) {
+			errors.rejectValue("stato", "dataRientro.not.exists.with.Stato.DISATTIVATO");
+		}
+	}
+
+}
